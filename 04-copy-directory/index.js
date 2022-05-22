2a7cc0b531e9508b60bf9e097002a3f7af537029
Okolokulak <71151545+Okolokulak@users.noreply.github.com>
2022-05-22 20:38:55 +0300

    feat: complete

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const fs = require("fs");
+const path = require("path");
+
+const dirCopy = path.join(__dirname, "files-copy")
+
+function copyDir(dir) {
+  fs.mkdir(dirCopy, { recursive: true }, () => {
+    fs.readdir(dir, (err, files) => {
+      files.forEach((file) => {
+        const origin = path.join(dir, file);
+        const destination = path.join(dirCopy, file);
+        fs.copyFile(origin, destination, () => {});
+      });
+    });
+  });
+}
+
+fs.stat(dirCopy, () => {
+  fs.rm(dirCopy, { recursive: true }, () => {
+    copyDir(path.join(__dirname, "files"));
+  });
+});
 
